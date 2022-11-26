3c3a92020c67d8635d477bc3237a1e8d1d192715
mverzi <maggie.verzi@gmail.com>
2022-11-26 12:07:55 -0500

    First commit

---
+++ invertValues.js
@@ -0,0 +1,8 @@
+/**
+ * This function inverts each value in a number array
+ * @param {array} array 
+ * @returns inverted array
+ */
+function invert(array) {
+    return array.map(n => n * -1)
+ }
